44866deb9e329ef83144c1d3c802d90429a126cb
monaiem2022 <95927663+monaiem2022@users.noreply.github.com>
2021-12-31 17:03:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+﻿
+
+
+
+
+
+
+window.onscroll = function () {
+    if(window.scrollY >=200){
+        navbar.style.backgroundColor = '#212529';
+        navbar.style.height = '65px';
+        navbarSupportedContent.style.backgroundColor = '#212529';
+    }
+    else{
+        navbar.style.backgroundColor = '';
+        navbarSupportedContent.style.backgroundColor = '';
+    }
+}
+
+
+
+
+
+
+
+window.onload = function () {
+    let web = new CountUp('web-count', 0 ,250 ,0 ,3);
+    web.start();
+    let app = new CountUp('app-count', 0 ,1000, 0, 3);
+    app.start();
+    let user = new CountUp('user-count',0 ,999 , 0, 3);
+    user.start();
+    let video = new CountUp('video-count',0 ,1325 , 0, 3);
+    video.start();
+ }
+
+
+
+
+document.getElementById('fullYear').innerHTML = new Date().getFullYear();
+
+
+$('.navbar-toggler').on('click',function(){
+    $('.navbar-collapse').css('background-color','#212529');
+});
